74a0627d74e594508dc20b5a39396ef65eb2b446
YasithCB <yasith.cb2001@gmail.com>
2022-10-07 13:02:00 +0530

    add animation to all of the section titles and give id's for all sections

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,4 +1,5 @@
 
+/*main title animation*/
 const text = document.querySelector('.fadeInText');
 const strText = text.textContent;
 const splitText = strText.split("");
@@ -25,4 +26,24 @@ function complete() {
     clearInterval(timer);
     timer = null;
 }
+/*main title animation*/
+
+
+/*section titles animation*/
+var sectionTitlesList = document.querySelectorAll('.sectionTitle'); /*get all titles to a list*/
+/*iterate for each title in list*/
+for (const title of sectionTitlesList) {
+    function scrollAppear() {
+        var sectionTitle = title;
+        var titlePosition = sectionTitle.getBoundingClientRect().top;
+        var screenPosition = window.innerHeight ;
+
+        if (titlePosition < screenPosition) {
+            sectionTitle.classList.add('sectionTitle-appear');
+        }
+    }
+    window.addEventListener('scroll',scrollAppear);
+}
+
+/*section titles animation*/
 
